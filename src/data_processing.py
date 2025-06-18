03b4c59e4c043991e80137273bd8baff92adfc66
wessantana <weslley.vieirasantana@gmail.com>
2025-06-18 04:41:25 -0300

    adiciona logs de funcionamento

--- src/data_processing.py
+++ src/data_processing.py
@@ -13,16 +13,21 @@ spark = SparkSession.builder.appName("AppSpark").config("spark.driver.host", "lo
     .getOrCreate()
 
     
-spark.sparkContext.setLogLevel("ERROR")
+spark.sparkContext.setLogLevel("WARN")
 
 def transformar_csv_em_dataframe(caminho: str):
+    try:
+        lista_csv = [f for f in os.listdir(caminho) if f.lower().endswith(".csv")]
+        lista_dataframes = []
 
-    lista_csv = [f for f in os.listdir(caminho) if f.lower().endswith(".csv")]
-    lista_dataframes = []
+        for nome_csv in lista_csv:
+            dataframe = spark.read.csv(f"{caminho}{nome_csv}", header=True, sep=";", encoding="ISO-8859-1", quote='"', escape='"')
+            lista_dataframes.append(dataframe)
+    except Exception as e:
+        print(f"Não foi possível converter o(s) CSV(s) em dataframe: {e}")
 
-    for nome_csv in lista_csv:
-        dataframe = spark.read.csv(f"{caminho}{nome_csv}", header=True, sep=";", encoding="ISO-8859-1", quote='"', escape='"')
-        lista_dataframes.append(dataframe)
+    print("O(s) CSV(s) foram convertidos com sucesso!")
+
     return lista_dataframes
 
 def padronizar_dataframe():
@@ -38,42 +43,44 @@ def padronizar_dataframe():
     lista_dataframes = transformar_csv_em_dataframe(caminho="data/raw/")
     dataframes_padronizados = []
 
-    for dataframe in lista_dataframes:
-        dataframe = dataframe.dropDuplicates()
+    try:
+        for dataframe in lista_dataframes:
+            dataframe = dataframe.dropDuplicates()
 
-        dataframe = dataframe.dropna(subset=["id", "data_inversa"])
+            dataframe = dataframe.dropna(subset=["id", "data_inversa"])
 
-        dataframe = dataframe.select([
-            trim(regexp_replace(col(c), r"[\n\r\t]", "")).alias(c) for c in dataframe.columns
-        ])
+            dataframe = dataframe.select([
+                trim(regexp_replace(col(c), r"[\n\r\t]", "")).alias(c) for c in dataframe.columns
+            ])
 
-        dataframe = dataframe.withColumn("data_inversa", to_date(col("data_inversa"), "yyyy-MM-dd"))
-        dataframe = dataframe.withColumn("horario", to_timestamp(col("horario"), "HH:mm:ss"))
+            dataframe = dataframe.withColumn("data_inversa", to_date(col("data_inversa"), "yyyy-MM-dd"))
+            dataframe = dataframe.withColumn("horario", to_timestamp(col("horario"), "HH:mm:ss"))
 
-        dataframe = dataframe.select([
-            when(col(c).isin("NA", "NA/NA", "Não Informado", "", "nan", "null", "None"), None)
-            .otherwise(col(c)).alias(c)
-            for c in dataframe.columns
-        ])
+            dataframe = dataframe.select([
+                when(col(c).isin("NA", "NA/NA", "Não Informado", "", "nan", "null", "None"), None)
+                .otherwise(col(c)).alias(c)
+                for c in dataframe.columns
+            ])
 
-        dataframe = dataframe.filter(col("latitude").isNotNull() & col("longitude").isNotNull())
+            dataframe = dataframe.filter(col("latitude").isNotNull() & col("longitude").isNotNull())
 
-        dataframe = dataframe.dropna(subset=["br", "km", "data_inversa"])
+            dataframe = dataframe.dropna(subset=["br", "km", "data_inversa"])
 
-        dataframe = dataframe.withColumn("km", regexp_replace(col("km"), ",", ".").cast(DoubleType()))
-        dataframe = dataframe.withColumn("latitude", regexp_replace(col("latitude"), ",", ".").cast(DoubleType()))
-        dataframe = dataframe.withColumn("longitude", regexp_replace(col("longitude"), ",", ".").cast(DoubleType()))
+            dataframe = dataframe.withColumn("km", regexp_replace(col("km"), ",", ".").cast(DoubleType()))
+            dataframe = dataframe.withColumn("latitude", regexp_replace(col("latitude"), ",", ".").cast(DoubleType()))
+            dataframe = dataframe.withColumn("longitude", regexp_replace(col("longitude"), ",", ".").cast(DoubleType()))
 
-        for coluna in colunas_numericas:
-            dataframe = dataframe.withColumn(coluna, col(coluna).cast(IntegerType()))
+            for coluna in colunas_numericas:
+                dataframe = dataframe.withColumn(coluna, col(coluna).cast(IntegerType()))
 
-        for coluna in colunas_texto:
-            dataframe = dataframe.withColumn(
-                coluna, when(col(coluna).isNull(), lit("unknown")).otherwise(col(coluna))
-            )
+            for coluna in colunas_texto:
+                dataframe = dataframe.withColumn(
+                    coluna, when(col(coluna).isNull(), lit("unknown")).otherwise(col(coluna))
+                )
 
-        dataframes_padronizados.append(dataframe)
-
+            dataframes_padronizados.append(dataframe)
+    except Exception as e:
+        print(f"Não foi possível padronizar o(s) dataframe(s): {e}")
     return dataframes_padronizados
 
 
@@ -81,12 +88,14 @@ def padronizar_dataframe():
 
 def salvar_dataframe(dataframes:list, caminho_para_salvar:str="data/processed/"):
 
-
-    lista_dataframes = dataframes
-    index_nome_arquivo = 1
-    for dataframe in lista_dataframes:
-        dataframe.repartition(10).write.mode("overwrite").option("header", True).parquet(f"{caminho_para_salvar}processed{index_nome_arquivo}")
-        index_nome_arquivo += 1
+    try:
+        lista_dataframes = dataframes
+        index_nome_arquivo = 1
+        for dataframe in lista_dataframes:
+            dataframe.repartition(10).write.mode("overwrite").option("header", True).parquet(f"{caminho_para_salvar}processed{index_nome_arquivo}")
+            index_nome_arquivo += 1
+    except Exception as e:
+        print(f"Não foi possível salvar o dataframe: {e}")
 
 
 try:
@@ -95,7 +104,8 @@ try:
     salvar_dataframe(dataframes_padronizados, "data/processed/")
 
 except Exception as e:
-    print(f"Não foi possível concluir a operação: {e}")
+    print(f"Não foi possível processar os dados: {e}")
+    
 finally:
     spark.stop()
 
